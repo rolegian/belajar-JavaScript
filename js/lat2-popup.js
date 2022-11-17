ebeccec72fc854b794af6cea2a8cff09f7ba636d
Rolegian <mail.alfren@gmail.com>
2022-11-18 06:12:06 +0700

    menambahkan file belajar pop up

---
+++ js/lat2-popup.js
@@ -0,0 +1,16 @@
+alert('hello world')
+var nama= prompt('masukkan nama');
+alert('hallo '+ nama);
+confirm('yakin Mas?');
+
+alert('selamat datang');
+var lop = true;
+
+while(lop === true){
+    var nama1 = prompt('Masukkan nama: ');
+    alert('hello Bang '+ nama1);
+
+    lop= confirm('mau coba lagi?');
+}
+
+alert('terimakasih');
