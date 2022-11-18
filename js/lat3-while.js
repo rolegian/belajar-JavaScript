bc48b14a86b5362f61acbf56a66eb0bc57fa0aca
Rolegian <mail.alfren@gmail.com>
2022-11-18 08:24:44 +0700

    membuat file while

---
+++ js/lat3-while.js
@@ -0,0 +1,11 @@
+var terus = true;
+while(terus){
+    console.log('Hello world');
+    terus = confirm('coba lagi?');
+}
+
+var nilaiAwal= 1;
+while(nilaiAwal<=3){
+    console.log('Hello world');
+    nilaiAwal++;
+}
