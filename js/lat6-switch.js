bb73884241b947b0b85db0a774ad80a2f2fa7e58
Rolegian <mail.alfren@gmail.com>
2022-11-18 12:46:55 +0700

    membuat file baru untuk switch js

---
+++ js/lat6-switch.js
@@ -0,0 +1,15 @@
+var item = prompt('masukkan nama makanan/minuman: \n(cth: nasi, daging');
+
+switch(item){
+    case 'nasi':
+        alert('makanan/minuman sehat');
+        break;
+    case 'ayam':
+        alert('makanan/minuman sehat');
+        break;
+    case 'besi':
+        alert('bukan makanan');
+        break;
+    default:
+        alert('masukkan makanan/minuman yang benar');
+}
