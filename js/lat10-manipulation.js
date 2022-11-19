2d0d9f2d835b63951fcb7dc35c5a2af180787731
Rolegian <mail.alfren@gmail.com>
2022-11-20 04:27:42 +0700

    manipulation part 1

---
+++ js/lat10-manipulation.js
@@ -0,0 +1,14 @@
+// const judul = document.getElementById('judul');
+// judul.innerHTML= 'Batutah Academy';
+
+// const sectionA = document.querySelector('section#a');
+// sectionA.innerHTML='Hello World'
+
+
+// const jusul = document.querySelector('#judul');
+// judul.style.color= 'salmon';
+
+
+const p2 = document.querySelector('.p2');
+p2.classList.add('name');
+p2.classList.remove('name');
--- js/lat9-DOMSelection.js
+++ js/lat9-DOMSelection.js
@@ -1,15 +1,26 @@
 //document.getElementById()
-const judul = document.getElementById('judul');
-judul.style.color = 'blue';
-judul.style.backgroundColor = '#fff';
-judul.innerHTML = "Belajar Javascript";
+//const judul = document.getElementById('judul');
+//judul.style.color = 'blue';
+//judul.style.backgroundColor = '#fff';
+//judul.innerHTML = "Belajar Javascript";
 
 
-const p = document.getElementsByTagName('p');
+//const p = document.getElementsByTagName('p');
 
-for( let i=0; i<p.length; i++){
-    p[i].style.backgroundColor= 'lightblue';
-}
+//for( let i=0; i<p.length; i++){
+//    p[i].style.backgroundColor= 'lightblue';
+//}
 
-const p1= document.getElementsByClassName('p1')[0];
-p1.innerHTML = 'Ini diubah dengan Javascript';
+//const p1= document.getElementsByClassName('p1')[0];
+//p1.innerHTML = 'Ini diubah dengan Javascript';//
+
+const p4= document.querySelector('#b p');
+p4.style.color= 'blue';
+p4.style.size= '25px';
+
+const li2 = document.querySelector('section#b ul li:nth-child(2)');
+
+li2.style.backgroundColor= 'orange';
+
+
+
