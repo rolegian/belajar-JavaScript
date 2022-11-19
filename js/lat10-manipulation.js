bba0a67eda059c65d1edbf8185271474d843f9b7
Rolegian <mail.alfren@gmail.com>
2022-11-20 04:49:38 +0700

    memodifikasi dan menambahkan kode baru

--- js/lat10-manipulation.js
+++ js/lat10-manipulation.js
@@ -9,6 +9,25 @@
 // judul.style.color= 'salmon';
 
 
-const p2 = document.querySelector('.p2');
-p2.classList.add('name');
-p2.classList.remove('name');
+// const p2 = document.querySelector('.p2');
+// p2.classList.add('name');
+// p2.classList.remove('name');
+
+// buat eleman baru
+const pBaru = document.createElement('p');
+const teksPBaru = document.createTextNode('Paragraf baru');
+// simpan tulisan ke dalam paragraf
+pBaru.appendChild(teksPBaru);
+
+// simpan di akhir section A
+const sectionA = document.getElementById('a');
+sectionA.appendChild(pBaru);
+
+const liBaru = document.createElement('li');
+const teksLiBaru = document.createTextNode('Item baru');
+liBaru.appendChild(teksLiBaru);
+
+const ul = document.querySelector('section#b ul');
+const li2 = ul.querySelector('li:nth-child(2)');
+
+ul.insertBefore(liBaru,li2);
