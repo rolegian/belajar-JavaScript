578337c023a0a573994c3926b9258ca477f2f5ce
Rolegian <mail.alfren@gmail.com>
2022-11-21 10:50:06 +0700

    menambah file lat13 image gallery

---
+++ js/lat13-image_galery.js
@@ -0,0 +1,13 @@
+const container = document.querySelector('.container');
+const jumbo = document.querySelector('.jumbo');
+
+container.addEventListener('click', function(e) {
+    // cek apakah yang diklik adalah thumb atau bukan
+    if(e.target.className == 'thumb') {
+        jumbo.src = e.target.src;
+        jumbo.classList.add('fade');
+        setTimeout(function(){
+            jumbo.classList.remove('fade');
+        }, 500)
+    }
+});
