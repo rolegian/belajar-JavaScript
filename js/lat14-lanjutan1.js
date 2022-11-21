e134682c27c36400d05e31ca1e76ec288163d69c
Rolegian <mail.alfren@gmail.com>
2022-11-22 05:54:54 +0700

    memodifikasi file lanjutan.js

--- js/lat14-lanjutan1.js
+++ js/lat14-lanjutan1.js
@@ -11,16 +11,77 @@
 // }
 
 // 2. Function declaration
-function mahasiswa(nama, energi){
-    let mahasiswa = {};
-    mahasiswa.nama = nama;
-    mahasiswa.energi = energi;
+// const methodMahasiswa = {
+//     makan: function(posi){
+//         this.energi += porsi ;
+//         console.log(`Haki ${this.nama}, selamat makan!`);
+//     },
 
-    mahasiswa.makan = function(porsi){
-        this.energi += porsi;
-        console.log(`halo ${this.nama}, selamat makana`);
-    }
-    return mahasiswa;
+//     main: function(jam){
+//         this.energi+= jam;
+//         console.log(`Halo ${this.nama}, selamat bermain!`);
+//     },
+
+//     tidur: function(jam){
+//         this.energi+= jam*2;
+//         console.log(`Halo ${this.nama}, selamat tidur!`);
+//     }
+// };
+
+// function Mahasiswa (nama, energi) {
+//     let mahasiswa = Object.create(methodMahasiswa);
+//     mahasiswa.nama= nama;
+//     mahasiswa.energi = energi;
+
+//     return mahasiswa;
+// }
+
+// let Asep = Mahasiswa('asep', 20);
+// let Ujang = Mahasiswa('Ujang', 10);
+
+
+// function Mahasiswa (nama, energi) {
+//     this.nama= nama;
+//     this.energi = energi;
+// }
+
+// Mahasiswa.prototype.makan= function(porsi){
+//     this.energi+= porsi;
+//     return `Halo ${this.nama}, selamat makan!`;
+// }
+
+// Mahasiswa.prototype.main= function(jam){
+//     this.energi -= jam;
+//     return `Halo ${this.nama}, selamat bermain!`;
+// }
+
+// Mahasiswa.prototype.tidur= function(jam){
+//     this.energi += jam *2;
+//     return `Halo ${this.nama}, selamat tidur!`;
+// }
+
+
+// Versi Class
+class Mahasiswa {
+    constructor(nama, energi) {
+    this.nama= nama;
+    this.energi = energi;
 }
 
-let asep = mahasiswa('asep', 10);
+  makan(porsi){
+    this.energi+= porsi;
+    return `Halo ${this.nama}, selamat makan!`;
+}
+
+  main(jam){
+    this.energi -= jam;
+    return `Halo ${this.nama}, selamat bermain!`;
+}
+
+  tidur(jam){
+    this.energi += jam *2;
+    return `Halo ${this.nama}, selamat tidur!`;
+}
+}
+
+let Ujang = new Mahasiswa('Ujang', 10);
